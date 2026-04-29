eaa9ed719c2bd6663231979d8620f6c9210aeaf0
Ruth <rut21178@gmail.com>
2026-04-29 12:15:27 +0300

    finalize CRUD API for one-time cancellation with proper module structure

--- room-scheduling-platform/server/index.js
+++ room-scheduling-platform/server/index.js
@@ -6,11 +6,14 @@ const PORT = process.env.PORT || 3000;
 const Room = require('./models/Room');
 const assignmentRoutes = require('./routes/assignmentRoutes');
 const cancellationRoutes = require('./routes/cancellationRoutes');
+import oneTimeCancellationRoutes from './routes/oneTimeCancellationRoutes.js';
 // Middleware
 app.use(cors()); // מאפשר ל-React לגשת לשרת
 app.use(express.json());
 app.use('/api/assignments', assignmentRoutes);
 app.use('/api/cancellations', cancellationRoutes);
+app.use('/api/oneTimeCancellations', oneTimeCancellationRoutes);
+
 // התחברות ל-MongoDB
 const uri = "mongodb+srv://tehila7151_db_user:bF8PM7JfYXqNlhHg@cluster0.8t6moga.mongodb.net/room-scheduling-platform?retryWrites=true&w=majority";
 
